792e316c07bc69c714201e48ff89d2dce97d1ac5
Marwen-Othmen <othmenmarwen@gmail.com>
2022-01-17 22:41:42 +0100

    checkpointjsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import foret from "./foret.jfif";
+import video from "./video.mp4";
+
+import "./style.css";
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+          <h1 className="titlered"> Marwen Othmen</h1>
+          <br />
+          <img className src={foret} />
+
+          <br />
+
+          <img src={"plage.jfif"} />
+        </div>
+
+        <video width={320} height={240} controls>
+          <source src={video}type="video/mp4" />
+        </video>
       </header>
     </div>
   );
